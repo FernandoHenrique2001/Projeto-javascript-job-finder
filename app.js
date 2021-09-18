d2ba17926c726e5a8336ec701a4ffa6169d5a85d
FernandoHenrique2001 <fernandohenrique.gp@gmail.com>
2021-09-18 15:43:37 -0300

    Criando a rota para adicionar registros

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const db = require("./db/connection.js");
+const bodyParser = require("body-parser");
 
 const PORT = 3000;
 
 app.listen(PORT, function(){
     console.log(`O Express está rodando na porta ${PORT}`);
 })
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false}))
 
 // db connection
 db
@@ -23,3 +27,6 @@ db
 app.get("/", (req, res) =>{
     res.send("Está funcionando");
 });
+
+// jobs routes
+app.use("/jobs", require("./routes/jobs"));
